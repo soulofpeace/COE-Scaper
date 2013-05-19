62a8a8a76bdede648d309b25b98ba170bf4777a4
Choon Kee Oh 胡俊齐 <choonkeeoh@Choons-MacBook-Air.local>
2013-05-20 00:23:34 +0800

    parsing pdf done

---
+++ pdf_parser.rb
@@ -0,0 +1,103 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'pdf-reader'
+require 'date'
+require 'json'
+
+def parse_coe(lines, current_index, date, map)
+  def parse_line(l, type, map, date)
+    tokens = l.split()
+    tokens[2..-1].each_with_index do |token, index|
+      if index % 2 == 0
+        category = case index
+                   when 0
+                     "A"
+                   when 2
+                     "B"
+                   when 4
+                     "C"
+                   when 6
+                     "D"
+                   when 8
+                     "E"
+                   end
+        date_first_bid = DateTime.new(date.year, date.month, 1, 0, 0, 0, 0)
+        date_second_bid = DateTime.new(date.year, date.month, 15, 0, 0, 0, 0)
+        map[category] ||= {} 
+        map[category][date_first_bid] ||= {}
+        map[category][date_first_bid][type] ||= {}
+        map[category][date_first_bid][type] = token
+        map[category][date_second_bid]||={}
+        map[category][date_second_bid][type] ||= {}
+        map[category][date_second_bid][type] = tokens[index+3]
+      end
+    end
+  end
+
+  count = 0 
+  offset = 0 
+  while count < 4
+    if lines[current_index+offset].strip != '' && lines[current_index+offset].strip.split.size >= 8
+      type = case count
+             when 0
+               'quota'
+             when 1
+               'successful'
+             when 2
+               'received'
+             when 3
+               'premium'
+             end
+
+      parse_line(lines[current_index+offset], type , map, date)
+      count = count + 1
+    end
+    offset = offset + 1
+  end
+  puts "map : #{JSON.pretty_generate(map)}"
+  map
+end
+
+def initialize_coe_map 
+  if File.exists? 'coe.json'
+    raw_coe_map = File.read('coe.json')
+    JSON.parse(raw_coe_map)
+  else
+    {}
+  end
+end
+
+inputfile = ARGV[0]
+map = initialize_coe_map
+
+
+File.open(inputfile, "rb") do |f|
+  reader = PDF::Reader.new(f)
+  reader.pages.each do |page|
+    text = page.text
+    lines = text.lines.map(&:chomp)
+    lines.each_with_index do |line, index|
+      begin 
+        line = line.strip
+        puts "#{index} -> #{line}"
+        date_string = line.split[0]
+        if (!date_string.nil?) and !(date_string=~/month/i) and date_string =~ /[\w]{3}-[\d]{4}/ and (!date_string.include?(")"))
+          date = DateTime.parse(date_string)
+          puts "Date: #{date}"
+          if date > DateTime.parse("Apr 2002")
+            parse_coe(lines, index, date, map)
+            File.open('coe.json', 'w') do |file|
+              file.write(map.to_json)
+            end
+          end
+        end
+      rescue ArgumentError  => e
+        #puts e.inspect
+      end
+    end
+  end
+end
+
+
+
+
--- scrape.py
+++ scrape.py
@@ -1,14 +1,8 @@
+import subprocess
 from scrapy import log
 from scrapy.selector import HtmlXPathSelector
 from scrapy.spider import BaseSpider
 from scrapy.http import Request
-from StringIO import StringIO
-import pyPdf
-from pdfminer.pdfparser import PDFDocument, PDFParser
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter, process_pdf
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdevice import PDFDevice, TagExtractor
 
 class COESpider(BaseSpider):
     name='coe'
@@ -26,22 +20,11 @@ class COESpider(BaseSpider):
 
     def parse_pdf(self, response):
         self.log('pdf %s response' % response.url)
-        #pdf = pyPdf.PdfFileReader(StringIO(response.body))
-        laparams = LAParams()
-        outtype = 'text'
-        codec = 'utf-8'
-        caching = True
-        srcmgr = PDFResourceManager(caching=caching)
-        outfp = StringIO()
-        maxpages = 0
-        password = ''
-        rsrcmgr = PDFResourceManager(caching=caching)
-        device = TextConverter(rsrcmgr, outfp, codec=codec, laparams=laparams)
-        process_pdf(rsrcmgr, device, StringIO(response.body), set(), maxpages=maxpages, password=password,
-                                    caching=caching, check_extractable=True)
+        filename_tokens = response.url.split("/")
+        filename = filename_tokens[len(filename_tokens)-1]
+        with open("pdfs/{0}".format(filename), 'wb') as f:
+            f.write(response.body)
 
-        #content=""
-        #for page in pdf.pages:
-            #content += page.extractText() + "\n"
-        #content = " ".join(content.replace(u"\xa0", " ").strip().split()) 
-        print outfp.getvalue()
+        subprocess.call("ruby pdf_parser.rb pdfs/{0}".format(filename), shell=True)
+
+
